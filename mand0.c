e2eacdf50fcebe7dd00c771e7e796c3c1b24bfa0
MoMoAxe <tobioluwa348@gmail.com>
2023-05-25 15:49:56 -0700

    mand0.c

--- mand0.c
+++ mand0.c
@@ -64,3 +64,21 @@ void _pall(_stack_t **stack, unsigned int line_number)
 		current = current->next;
 	}
 }
+
+/**
+ * _pint - this function prints the value at the top of the stack
+ * @stack: A pointer to stack
+ * @line_number: The line number
+ * Return: NULL or void
+ */
+void _pint(_stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_all(*stack, global.file);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
